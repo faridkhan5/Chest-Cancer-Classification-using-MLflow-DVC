4646455b25bfa93aee90a6089e5fcd9f1b5bb90c
faridkhan5 <faridk302@gmail.com>
2024-04-22 00:47:43 +0530

    dvc added

--- README.md
+++ README.md
@@ -12,3 +12,24 @@
 7. Update the pipeline
 8. Update the main.py
 9. Update the dvc.yaml
+
+
+### Dagshub
+
+MLFLOW_TRACKING_URI=https://dagshub.com/faridkhan5/Chest-Cancer-Classification-using-MLflow-DVC.mlflow \
+MLFLOW_TRACKING_USERNAME=faridkhan5 \
+MLFLOW_TRACKING_PASSWORD=801f7f4defd33f302dd86616acda87ee94a5d725 \
+python script.py
+
+Run this to export as env variables:
+
+```bash
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/faridkhan5/Chest-Cancer-Classification-using-MLflow-DVC.mlflow
+
+export MLFLOW_TRACKING_USERNAME=faridkhan5
+
+export MLFLOW_TRACKING_PASSWORD=801f7f4defd33f302dd86616acda87ee94a5d725
+
+```
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = 'Data Ingestion stage'
@@ -11,7 +12,7 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx============x")
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx============x\n\n")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -25,7 +26,7 @@ try:
     
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx========x")
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx========x\n\n")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -39,7 +40,22 @@ try:
     
     obj = ModelTrainingPipeline()
     obj.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx========x")
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx========x\n\n")
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = 'Evaluation Stage'
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"**************")
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+    
+    except Exception as e:
+        logger.exception(e)
+        raise e
